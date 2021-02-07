72cacc3a4cf195e951f06d31e1ea88261f8e3bfe
Alkaede <andrewcast32@gmail.com>
2021-02-07 13:31:53 -0800

    api fetch done, component setup

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,55 @@
 // Write your Character component here
+import React, { useState } from 'react'
+import axios from 'axios'
+// import styled from 'styled-components'
+
+// const charContainer = styled.div`
+
+//     ul{
+//         list-style-type: none;
+//     }
+
+// `
+
+
+
+
+export default function CharacterGrabber() {
+
+    const [characters, setCharacters] = useState(null) 
+
+
+    // setting up axios to fetch character data
+    axios
+        .get('https://swapi.dev/api/people/')
+        .then(res => {
+            console.log(res.data)
+            setCharacters(res.data)
+        })
+        .catch(err => console.log(err))
+
+
+        return (
+            <div>
+            {
+                characters && 
+                    characters.map((character) => {
+                        return (
+                            <div>
+                                <div>
+                                    <h2>{character.name}</h2>
+                                    <h2>{character.birth_year}</h2>
+                                </div>
+                                    <ul>
+                                        <li>Gender: {character.gender}</li>
+                                        <li>Mass: {character.mass}</li>
+                                        <li>Eye Color: {character.eye_color}</li>
+                                        <li>Hair Color: {character.hair_color}</li>
+                                    </ul>
+                            </div>
+                        )
+                    })
+            }
+            </div>
+        )
+    }
