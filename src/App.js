0213d3f8290b3780d8ecdab7ca67214ca681ff00
Alkaede <andrewcast32@gmail.com>
2021-02-07 14:34:03 -0800

    styled-components and adjust some lines of code/importing font family to css, maybe will come back to add props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import './App.css';
 import CharacterGrabber from './components/Character';
+import styled from 'styled-components'
+
 
 const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <img src='http://imageshack.com/a/img922/3783/oyvsRd.png' /> 
+      <Header>
+      Characters  
+      </Header>
       <CharacterGrabber />
     </div>
   );
 }
 
 export default App;
+
+
+const Header = styled.h1`
+  color: #FFE81F;
+  font-size: 3.5rem;
+`
+
+
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,55 +1,77 @@
 // Write your Character component here
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-// import styled from 'styled-components'
-
-// const charContainer = styled.div`
-
-//     ul{
-//         list-style-type: none;
-//     }
-
-// `
-
-
+import styled from 'styled-components'
 
 
 export default function CharacterGrabber() {
 
     const [characters, setCharacters] = useState(null) 
 
-
     // setting up axios to fetch character data
-    axios
+    useEffect(()=>{
+        axios
         .get('https://swapi.dev/api/people/')
         .then(res => {
             console.log(res.data)
             setCharacters(res.data)
         })
         .catch(err => console.log(err))
+    }, [])
+
 
 
         return (
-            <div>
+            <CharContainer>
             {
                 characters && 
                     characters.map((character) => {
                         return (
                             <div>
-                                <div>
-                                    <h2>{character.name}</h2>
-                                    <h2>{character.birth_year}</h2>
-                                </div>
-                                    <ul>
-                                        <li>Gender: {character.gender}</li>
-                                        <li>Mass: {character.mass}</li>
-                                        <li>Eye Color: {character.eye_color}</li>
-                                        <li>Hair Color: {character.hair_color}</li>
-                                    </ul>
+                                <details>
+                                    <summary>
+                                        <h2>{character.name}</h2>
+                                    </summary>
+                                        <ul>
+                                            <li>Year Born: {character.birth_year}</li>
+                                            <li>Gender: {character.gender}</li>
+                                            <li>Mass: {character.mass}</li>
+                                            <li>Eye Color: {character.eye_color}</li>
+                                            <li>Hair Color: {character.hair_color}</li>
+                                        </ul>
+                                </details>
                             </div>
                         )
                     })
             }
-            </div>
+            </CharContainer>
         )
     }
+
+
+    const CharContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    margin: 5%;
+    color: #ededed;
+    text-shadow: 1px 1px 5px #fff;
+
+        h2{
+        display: inline-block;
+        }
+
+        ul{
+        display: flex;
+        flex-flow: column wrap;
+        justify-content: space-evenly;
+        list-style-type: none;
+        font-weight: bold;
+        font-size: 1.5rem;
+        padding-right: 35px;
+        }
+
+        li{
+        padding: 1vh;
+        }
+    `
