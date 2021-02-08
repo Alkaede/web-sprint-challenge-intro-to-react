4c62976512990d7139e7a06146388bd6b55d1cf1
Alkaede <andrewcast32@gmail.com>
2021-02-07 16:17:23 -0800

    fixed files with props

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import React from 'react';
-import './App.css';
-import CharacterGrabber from './components/Character';
+import React, { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios'
 import styled from 'styled-components'
+import Character from './components/Character'
 
 
 const App = () => {
 
+  const [characters, setCharacters] = useState([]); 
+
+  // setting up axios to fetch character data
+  useEffect(()=>{
+      axios
+      .get('https://swapi.dev/api/people/')
+      .then(res => {
+        setCharacters(res.data);
+      })
+      .catch(err => {console.log(err)})
+  }, []);
+
+
   return (
     <div className="App">
-      <img src='http://imageshack.com/a/img922/3783/oyvsRd.png' /> 
+      <img src='http://imageshack.com/a/img922/3783/oyvsRd.png' alt='starwars' /> 
       <Header>
       Characters  
       </Header>
-      <CharacterGrabber />
+       <div>
+       { characters && characters.map(character => {
+          return(<Character key={character.id} props= {character}/>)
+        })}
+       </div>
     </div>
-  );
+  )
 }
-
 export default App;
 
 
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,78 +1,63 @@
-// Write your Character component here
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+// Write your props component here
+import React from 'react'
 import styled from 'styled-components'
 
+// interface IProps{
+//     name: String;
+//     gender: String;
+// }
 
-export default function CharacterGrabber() {
-
-    const [characters, setCharacters] = useState(null) 
-
-    // setting up axios to fetch character data
-    useEffect(()=>{
-        axios
-        .get('https://swapi.dev/api/people/')
-        .then(res => {
-            console.log(res.data)
-            setCharacters(res.data)
-        })
-        .catch(err => console.log(err))
-    }, [])
-
-
-
-        return (
-            <CharContainer>
-            {
-                characters && 
-                    characters.map((character) => {
-                        return (
-                            <div>
-                                <details>
-                                    <summary>
-                                        <h2>{character.name}</h2>
-                                    </summary>
-                                        <ul>
-                                            <li>Year Born: {character.birth_year}</li>
-                                            <li>Gender: {character.gender}</li>
-                                            <li>Height: {character.height}</li>
-                                            <li>Mass: {character.mass}</li>
-                                            <li>Eye Color: {character.eye_color}</li>
-                                            <li>Hair Color: {character.hair_color}</li>
-                                        </ul>
-                                </details>
-                            </div>
-                        )
-                    })
-            }
-            </CharContainer>
-        )
-    }
+    const Character = ({props}) => {
+        return (       
+                <CharContainer>
+                        <div>
+                            <details>
+                                <summary>
+                                    <h2>{props.name}</h2>
+                                </summary>
+                                <div>
+                                    <ul>
+                                        <li>Year Born: {props.birth_year}</li>
+                                        <li>Gender: {props.gender}</li>
+                                        <li>Height: {props.height}</li>
+                                        <li>Mass: {props.mass}</li>
+                                        <li>Eye Color: {props.eye_color}</li>
+                                        <li>Hair Color: {props.hair_color}</li>
+                                    </ul>
+                                </div>
+                            </details>
+                        </div>
+                </CharContainer>
+    )
+}
 
 
     const CharContainer = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-    margin: 5%;
-    color: #ededed;
-    text-shadow: 1px 1px 5px #fff;
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-evenly;
+            align-items: center;
+            margin: 5%;
+            color: #ededed;
+            text-shadow: 1px 1px 5px #fff;
 
-        h2{
-        display: inline-block;
-        }
+            h2{
+            display: inline-block;
+            }
 
-        ul{
-        display: flex;
-        flex-flow: column wrap;
-        justify-content: space-evenly;
-        list-style-type: none;
-        font-weight: bold;
-        font-size: 1.5rem;
-        padding-right: 35px;
-        }
+            ul{
+            display: flex;
+            flex-flow: column wrap;
+            justify-content: space-evenly;
+            list-style-type: none;
+            font-weight: bold;
+            font-size: 1.5rem;
+            padding-right: 35px;
+            }
 
-        li{
-        padding: 1vh;
-        }
+            li{
+            padding: 1vh;
+            }
     `
+
+export default Character
